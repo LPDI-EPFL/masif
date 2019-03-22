9a2f999979b5c7ccfcfbca5bad13d700fe819be7
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-22 16:52:09 +0100

    Added neural networks default directories.

---
+++ data/masif_ppi_search/nn_models/sc05/all_feat/custom_params.py
@@ -0,0 +1,5 @@
+custom_params = {}
+custom_params['cache_dir'] = 'nn_models/sc05/cache/'
+custom_params['model_dir'] = 'nn_models/sc05/all_feat/'
+masif_opts['ppi_search']['desc_dir'] = 'descriptors/sc05/all_feat/'
+masif_opts['ppi_search']['feat_mask'] = [1.0, 1.0, 1.0, 1.0, 1.0]
---
+++ data/masif_ppi_search/nn_models/sc05/custom_params.py
@@ -0,0 +1,5 @@
+custom_params = {}
+custom_params['cache_dir'] = 'nn_models/sc05/cache/'
+custom_params['min_sc_filt'] = 0.5
+custom_params['max_sc_filt'] = 1.0
+custom_params['pos_surf_accept_probability'] = 1.0
