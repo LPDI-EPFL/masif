9cf40f5bee794d5cbed2ed2b7a9ac22ed759e1c5
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-22 16:24:33 +0100

    Minor name changes.

--- data/masif_ppi_search/train_nn.sh
+++ data/masif_ppi_search/train_nn.sh
@@ -3,4 +3,4 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 #source /home/gainza/lpdi_fs/seeder/data/ppi_benchmark_complexes/tensorflow-1.9/bin/activate
 #python $masif_source/05-masif_ppi_cache_training_data.py 
-python $masif_source/06-masif_ppi_search_train.py
+python $masif_source/06-masif_ppi_search_train.py $1
