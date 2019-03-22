78bcecd431d9e9ccc54877739098506a22e9bd0c
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-22 16:25:32 +0100

    Added run script for cache_nn

---
+++ data/masif_ppi_search/cache_nn.sh
@@ -0,0 +1,6 @@
+masif_root=$(git rev-parse --show-toplevel)
+masif_source=$masif_root/source/
+masif_data=$masif_root/data/
+masif_matlab=$masif_root/source/matlab_libs/
+export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
+python $masif_source/masif_ppi_search/masif_ppi_search_cache_training_data.py $1
