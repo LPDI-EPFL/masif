84589f858431efe5be135be065bf95dc71c5edc2
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-22 13:08:16 +0100

    Cleaned up file.

--- source/core/initialize_matlab.py
+++ source/core/initialize_matlab.py
@@ -8,8 +8,5 @@ def initialize_matlab():
     eng.addpath(matlab_root+'/fmm/',nargout=0)
     eng.addpath(matlab_root+'/util/',nargout=0)
     eng.addpath(matlab_root+'/coords/',nargout=0)
-#    eng.addpath(r'~/cur/seeder/source/matlab_libs/fmm/',nargout=0)
-#    eng.addpath(r'~/cur/seeder/source/matlab_libs/fmm/toolbox_fast_marching',nargout=0)
-#    eng.addpath(r'~/cur/seeder/source/matlab_libs/bindomers/',nargout=0)
     return eng
 
