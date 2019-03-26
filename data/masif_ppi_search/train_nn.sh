73ccba7c4914b1038087668518f38ec5ea3c19f1
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-26 12:45:15 +0100

    Updated script with no absolute paths.

--- data/masif_ppi_search/train_nn.sh
+++ data/masif_ppi_search/train_nn.sh
@@ -1,6 +1,6 @@
 masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
-#source /home/gainza/lpdi_fs/seeder/data/ppi_benchmark_complexes/tensorflow-1.9/bin/activate
-#python $masif_source/05-masif_ppi_cache_training_data.py 
-python $masif_source/06-masif_ppi_search_train.py $1
+masif_data=$masif_root/data/
+export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
+python $masif_source/masif_ppi_search/masif_ppi_search_train.py $1
