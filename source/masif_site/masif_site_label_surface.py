a969f023d1ed86d468d6d46de89ed91f409cd244
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-29 14:41:30 +0100

    Added evaluation and labeling code to the output

---
+++ source/masif_site/masif_site_label_surface.py
@@ -0,0 +1,89 @@
+import pymesh
+import os
+import sys
+import importlib
+import numpy as np
+import ipdb
+from default_config.masif_opts import masif_opts
+
+from masif_modules.read_data_from_matfile import load_matlab_file
+
+params = masif_opts['site']
+custom_params_file = sys.argv[1]
+custom_params = importlib.import_module(custom_params_file, package=None)
+custom_params = custom_params.custom_params
+
+for key in custom_params: 
+    print('Setting {} to {} '.format(key, custom_params[key]))
+    params[key] = custom_params[key]
+
+# Shape precomputation dir.
+parent_in_dir = params['masif_precomputation_dir']
+eval_list = []
+
+if len(sys.argv) == 3:
+    eval_list = [sys.argv[2]]
+# Read a list of pdb_chain entries to evaluate. 
+if len(sys.argv) == 4 and sys.argv[2] == '-l':
+    listfile = open(sys.argv[3])
+    ppi_pair_ids = []
+    for line in listfile: 
+        eval_list.append(line.rstrip())
+else:
+    sys.exit(1)
+
+for mydir in os.listdir(parent_in_dir):
+    ppi_pair_ids.append(mydir)
+
+for ppi_pair_id in ppi_pair_ids:
+    shape_file = masif_opts['mat_dir']+ppi_pair_id+'/'+ppi_pair_id+'.mat'
+    pdbid = ppi_pair_id.split('_')[0]
+    chains = [ppi_pair_id.split('_')[1], ppi_pair_id.split('_')[2]]
+
+    for ix, pid in enumerate(['p1', 'p2']):
+        pdb_chain_id = pdbid+'_'+chains[ix]
+
+        if pdb_chain_id not in eval_list:
+            continue
+
+        
+        try:
+            p1 = load_matlab_file(shape_file, pid, True)
+        except:
+            ipdb.set_trace()
+        scores = np.load(params['out_pred_dir']+'/pred_'+pdbid+'_'+chains[ix]+'.npy')
+
+        vertices = np.stack([p1['X'][0], p1['Y'][0], p1['Z'][0]], axis=1)
+        faces = p1['TRIV'].T
+        # Matlab indexing starts at 1.
+        faces = faces - 1
+
+        mymesh = pymesh.form_mesh(vertices, faces)
+        mymesh.add_attribute('charge')
+        mymesh.set_attribute('charge', p1['charge'][0])
+
+        normals = p1['normal']
+        n1 = normals[0,:]
+        n2 = normals[1,:]
+        n3 = normals[2,:]
+        mymesh.add_attribute('vertex_nx')
+        mymesh.set_attribute('vertex_nx', n1)
+        mymesh.add_attribute('vertex_ny')
+        mymesh.set_attribute('vertex_ny', n2)
+        mymesh.add_attribute('vertex_nz')
+        mymesh.set_attribute('vertex_nz', n3)
+
+        mymesh.add_attribute('hphob')
+        mymesh.set_attribute('hphob', p1['hphob'][0])
+
+        mymesh.add_attribute('hbond')
+        mymesh.set_attribute('hbond', p1['hbond'][0])
+
+        mymesh.add_attribute('iface')
+        mymesh.set_attribute('iface', scores[0])
+
+        if not os.path.exists(params['out_surf_dir']):
+            os.makedirs(params['out_surf_dir'])
+
+        pymesh.save_mesh(params['out_surf_dir']+pdb_chain_id+'.ply', mymesh, *mymesh.get_attribute_names(), use_float=True, ascii=True)
+
---
+++ source/masif_site/masif_site_predict.py
@@ -0,0 +1,89 @@
+# Header variables and parameters.
+import os
+import numpy as np
+from IPython.core.debugger import set_trace
+import sys
+import importlib
+from masif_modules.train_masif_site import run_masif_site 
+from default_config.masif_opts import masif_opts
+
+# Apply mask to input_feat
+def mask_input_feat(input_feat, mask):
+    mymask = np.where(np.array(mask) == 0.0)[0]
+    return np.delete(input_feat, mymask, axis=2)
+
+params = masif_opts['site']
+custom_params_file = sys.argv[1]
+custom_params = importlib.import_module(custom_params_file, package=None)
+custom_params = custom_params.custom_params
+
+for key in custom_params: 
+    print('Setting {} to {} '.format(key, custom_params[key]))
+    params[key] = custom_params[key]
+
+
+# Shape precomputation dir.
+parent_in_dir = params['masif_precomputation_dir']
+eval_list = []
+
+if len(sys.argv) == 3:
+    ppi_pair_ids = [sys.argv[2]]
+# Read a list of pdb_chain entries to evaluate. 
+elif len(sys.argv) == 4 and sys.argv[2] == '-l':
+    listfile = open(sys.argv[3])
+    ppi_pair_ids = []
+    for line in listfile: 
+        eval_list.append(line.rstrip())
+    for mydir in os.listdir(parent_in_dir):
+        ppi_pair_ids.append(mydir)
+else:
+    sys.exit(1)
+
+# Build the neural network model 
+from masif_modules.MaSIF_site import MaSIF_site
+
+learning_obj = MaSIF_site(params['max_distance'], n_thetas=4, n_rhos=3, n_rotations=4, idx_gpu = '/gpu:0', feat_mask=params['feat_mask'], n_conv_layers=params['n_conv_layers'] )
+print('Restoring model from: '+params['model_dir']+'model')
+learning_obj.saver.restore(learning_obj.session, params['model_dir']+'model')
+
+if not os.path.exists(params['out_pred_dir']):
+    os.makedirs(params['out_pred_dir'])
+
+idx_count = 0
+for ppi_pair_id in ppi_pair_ids:
+    print(ppi_pair_id)
+    in_dir = parent_in_dir + ppi_pair_id+'/'
+    
+    pdbid = ppi_pair_id.split('_')[0]
+    chain1 = ppi_pair_id.split('_')[1]
+    chain2 = ppi_pair_id.split('_')[2]
+    pids = ['p1']
+    chains = [chain1]
+    if chain2 != '':
+        pids = ['p1', 'p2']
+        chains = [chain1, chain2]
+    
+    for ix, pid in enumerate(pids):
+        pdb_chain_id = pdbid+'_'+chains[ix]
+        if len(eval_list) > 0  and pdb_chain_id not in eval_list:
+            continue
+        
+        print('Evaluating {}'.format(pdb_chain_id))
+
+        try:
+            rho_wrt_center = np.load(in_dir+pid+'_rho_wrt_center.npy')
+        except:
+            continue
+        theta_wrt_center = np.load(in_dir+pid+'_theta_wrt_center.npy')
+        input_feat = np.load(in_dir+pid+'_input_feat.npy')
+        input_feat = mask_input_feat(input_feat, params['feat_mask'])
+        mask = np.load(in_dir+pid+'_mask.npy')
+        indices = np.load(in_dir+pid+'_list_indices.npy', encoding='latin1')
+        labels = np.zeros((len(mask)))
+
+        print ("Total number of patches:{} \n".format(len(mask)))
+
+        scores = run_masif_site(params, learning_obj, rho_wrt_center, theta_wrt_center, input_feat, mask, indices)
+        print ("Total number of patches for which scores were computed: {}\n".format(len(scores)))
+        np.save(params['out_pred_dir']+'/pred_'+pdbid+'_'+chains[ix]+'.npy', scores)
+
