b7bc958493a56b502533caa28c3612af757b532d
Pablo Gainza <pablo.gainza@epfl.ch>
2019-03-29 14:43:27 +0100

    Updated masif_ppi_search data directories.

--- data/masif_ppi_search/data_prepare_one.sh
+++ data/masif_ppi_search/data_prepare_one.sh
@@ -9,7 +9,7 @@ CHAIN2=$(echo $1| cut -d"_" -f3)
 #python $masif_source/data_preparation/00-pdb_download.py $1
 #python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
 #python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
-#python $masif_source/data_preparation/02-compute_matlab_matrix.py $1
+python $masif_source/data_preparation/02-compute_matlab_matrix.py $1
 #python $masif_source/data_preparation/03-compute_coords.py $1
 #python $masif_source/data_preparation/03b-convert_mat2npy.py $1
 python $masif_source/data_preparation/04-masif_precompute.py masif_ppi_search $1
