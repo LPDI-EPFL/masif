e48672f2f8f9c1cb81286b4be6057a407298853d
Pablo Gainza <pablo.gainza@epfl.ch>
2019-04-01 11:19:51 +0200

    Cleaned up.

--- data/masif_ppi_search/compute_descriptors.sh
+++ data/masif_ppi_search/compute_descriptors.sh
@@ -3,6 +3,5 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
-#source /home/gainza/lpdi_fs/seeder/data/ppi_benchmark_complexes/tensorflow-1.9/bin/activate
+#source $masif_source/tensorflow-1.9/bin/activate
 python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py $1 -l lists/full_list.txt
-#python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py $1 4M5F_A_B
