bc858fe00bc3ec8440ae83ebfb17a17d2b2d74fa
Pablo Gainza <pablo.gainza@epfl.ch>
2019-04-17 13:05:54 +0200

    Added pdl1 benchmark.

--- data/masif_ppi_search/compute_descriptors.sh
+++ data/masif_ppi_search/compute_descriptors.sh
@@ -4,4 +4,4 @@ masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
 #source $masif_source/tensorflow-1.9/bin/activate
-python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py $1 -l lists/full_list.txt
+python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py nn_models.sc05.all_feat.custom_params -l $1
