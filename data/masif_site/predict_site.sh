a1d407804caa9920f3210323d9631414e55fae67
Pablo Gainza <pablo.gainza@epfl.ch>
2019-04-24 14:06:43 +0200

    minor changes in data files.

--- data/masif_site/predict_site.sh
+++ data/masif_site/predict_site.sh
@@ -3,4 +3,4 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l.custom_params -l lists/full_list_expanded.txt
+python $masif_source/masif_site/masif_site_label_surface.py nn_models.all_feat_3l.custom_params -l lists/testing_transient.txt
