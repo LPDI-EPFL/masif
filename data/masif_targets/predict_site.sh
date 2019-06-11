b615152e474f166a01c462352120c9c7d33da430
Pablo Gainza <pablo.gainza@epfl.ch>
2019-06-11 11:22:05 +0200

    Updated masif_targets directory.

--- data/masif_targets/predict_site.sh
+++ data/masif_targets/predict_site.sh
@@ -3,6 +3,5 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-#python $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l.custom_params -l lists/pdl1_pd1.txt
-#python $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l.custom_params -l lists/crsas6.zh.list
+#python $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l.custom_params -l lists/mylist.txt
 python $masif_source/masif_site/masif_site_predict.py nn_models.all_feat_3l.custom_params $1
