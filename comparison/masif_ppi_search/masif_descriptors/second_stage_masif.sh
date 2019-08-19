fc8eafaaf2afe7800fff1ddd9f06607309ce59bb
Pablo Gainza <pablo.gainza@epfl.ch>
2019-08-19 11:50:06 +0200

    Second stage masif with no neural network ranking.

--- comparison/masif_ppi_search/masif_descriptors/second_stage_masif.sh
+++ comparison/masif_ppi_search/masif_descriptors/second_stage_masif.sh
@@ -3,7 +3,8 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 100 2000 100 masif
-python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 500 2000 100 masif
-python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 1000 2000 100 masif
-python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 3000 2000 100 masif
+source /home/gainza/lpdi_fs/masif/tensorflow-1.12_on_cpu/bin/activate
+python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 100 4000 1000 masif
+python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 1000 4000 1000 masif
+python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 2000 4000 1000 masif
+python $masif_source/masif_ppi_search/second_stage_alignment.py ../../../data/masif_ppi_search 3000 4000 1000 masif
