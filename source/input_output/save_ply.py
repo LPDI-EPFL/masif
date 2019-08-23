2b66259109687f878b42dab3ce755d9f3ee7ed1d
Pablo Gainza <pablo.gainza@epfl.ch>
2019-08-23 11:37:19 +0200

    Update save_ply.py

--- source/input_output/save_ply.py
+++ source/input_output/save_ply.py
@@ -1,5 +1,10 @@
 import pymesh
 import numpy
+"""
+read_ply.py: Save a ply file to disk using pymesh and load the attributes used by MaSIF. 
+Pablo Gainza - LPDI STI EPFL 2019
+Released under an Apache License 2.0
+"""
 
 
 def save_ply(
