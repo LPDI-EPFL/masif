347b0d251848c5d6a968e1570020ebc4b02194f9
Pablo Gainza <pablo.gainza@epfl.ch>
2019-08-23 11:07:55 +0200

    Updated header

--- source/input_output/protonate.py
+++ source/input_output/protonate.py
@@ -1,11 +1,19 @@
-# Protonate a pdb using the reduce program
+"""
+protonate.py: Wrapper method for the reduce program: protonate a pdb using reduce 
+                and save to an output file.
+Pablo Gainza - LPDI STI EPFL 2019
+Released under an Apache License 2.0
+"""
+
 from subprocess import Popen, PIPE
 import os
 from ipdb import set_trace
 
 
 def protonate(in_pdb_file, out_pdb_file):
-
+    # in_pdb_file: file to protonate.
+    # out_pdb_file: output file where to save the protonated pdb file. 
+    
     # Remove protons first, in case the structure is already protonated
     args = ["reduce", "-Trim", in_pdb_file]
     p2 = Popen(args, stdout=PIPE, stderr=PIPE)
