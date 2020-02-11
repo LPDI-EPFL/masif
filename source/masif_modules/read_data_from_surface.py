212f6b1f722597ac34c290dca5ad366956077665
Pablo Gainza <pablo.gainza@epfl.ch>
2020-02-11 16:14:17 +0100

    Compute shape complementarity of patches.

--- source/masif_modules/read_data_from_surface.py
+++ source/masif_modules/read_data_from_surface.py
@@ -2,7 +2,6 @@
 # ## Imports and helper functions
 from IPython.core.debugger import set_trace
 import pymesh
-import numpy as np
 import time
 import numpy as np
 
@@ -95,6 +94,7 @@ def extract_patch_and_coord(
     # Member vertices are nonzero elements
     i, j = coord[np.int(vix), : coord.shape[1] // 2].nonzero()
 
+
     # D = np.squeeze(np.asarray(coord[np.int(vix),j].todense()))
     D = np.squeeze(np.asarray(coord[np.int(vix), : coord.shape[1] // 2].todense()))
     j = np.where((D < max_distance) & (D > 0))[0]
@@ -128,37 +128,121 @@ def extract_patch_and_coord(
         return patch, coord
 
 
-# FOR DEBUGGING only.... too slow, use precomputed values instead.
-from scipy.spatial import KDTree
-def compute_shape_complementarity(shape1, coord1, shape2, coord2):
-    w = 0.5
-    radius = 8.0
+from scipy.spatial import cKDTree
+# neigh1 and neigh2 are the precomputed indices; rho1 and rho2 their distances.
+def compute_shape_complementarity(ply_fn1, ply_fn2, neigh1, neigh2, rho1, rho2, mask1, mask2, params):
+    """
+        compute_shape_complementarity: compute the shape complementarity between all pairs of patches. 
+        ply_fnX: path to the ply file of the surface of protein X=1 and X=2
+        neighX, rhoX, maskX: (N,max_vertices_per_patch) matrices with the indices of the neighbors, the distances to the center 
+                and the mask
 
-    D1 = coord1[: coord1.shape[0] // 2]
-    v1 = np.stack([shape1["X"], shape1["Y"], shape1["Z"]], 1)
-    v1 = v1[np.where(D1 < radius)]
-    n1 = shape1["normal"].T[np.where(D1 < radius)]
+        Returns: vX_sc (2,N,10) matrix with the shape complementarity (shape complementarity 25 and 50) 
+        of each vertex to its nearest neighbor in the other protein, in 10 rings.
+    """
+    # Mesh 1
+    mesh1 = pymesh.load_mesh(ply_fn1)
+    # Normals: 
+    nx = mesh1.get_attribute("vertex_nx")
+    ny = mesh1.get_attribute("vertex_ny")
+    nz = mesh1.get_attribute("vertex_nz")
+    n1 = np.stack([nx,ny,nz], axis=1)
 
-    D2 = coord2[: coord2.shape[0] // 2]
-    v2 = np.stack([shape2["X"], shape2["Y"], shape2["Z"]], 1)
-    v2 = v2[np.where(D2 < radius)]
-    n2 = shape2["normal"].T[np.where(D2 < radius)]
+    # Mesh 2
+    mesh2 = pymesh.load_mesh(ply_fn2)
+    # Normals: 
+    nx = mesh2.get_attribute("vertex_nx")
+    ny = mesh2.get_attribute("vertex_ny")
+    nz = mesh2.get_attribute("vertex_nz")
+    n2 = np.stack([nx,ny,nz], axis=1)
 
-    # First v2 -> v1
-    kdt = KDTree(v1)
-    d, i = kdt.query(v2)
-    comp2 = [np.dot(n2[x], -n1[i[x]]) for x in range(len(n2))]
-    comp2 = np.multiply(comp2, np.exp(-w * np.square(d)))
-    comp2 = np.percentile(comp2, 50)
+    w = params['sc_w']
+    int_cutoff = params['sc_interaction_cutoff']
+    radius = params['sc_radius']
+    num_rings = 10
+    scales = np.arange(0, radius, radius/10)
+    scales = np.append(scales, radius)
 
-    # Now v1 -> v2
-    kdt = KDTree(v2)
-    d, i = kdt.query(v1)
-    comp1 = [np.dot(n1[x], -n2[i[x]]) for x in range(len(n1))]
-    comp1 = np.multiply(comp1, np.exp(-w * np.square(d)))
-    comp1 = np.percentile(comp1, 50)
+    v1 = mesh1.vertices
+    v2 = mesh2.vertices
 
-    return np.mean([comp1, comp2])
+    v1_sc = np.zeros((2,len(v1), 10))
+    v2_sc = np.zeros((2,len(v2), 10))
+
+    # Find all interface vertices
+    kdt = cKDTree(v2)
+    d, nearest_neighbors_v1_to_v2 = kdt.query(v1)
+    # Interface vertices in v1
+    interface_vertices_v1 = np.where(d < int_cutoff)[0]
+
+    # Go through every interface vertex. 
+    for cv1_iiix in range(len(interface_vertices_v1)):
+        cv1_ix = interface_vertices_v1[cv1_iiix]
+        assert (d[cv1_ix] < int_cutoff)
+        # First shape complementarity s1->s2 for the entire patch
+        patch_idxs1 = np.where(mask1[cv1_ix]==1)[0]
+        neigh_cv1 = np.array(neigh1[cv1_ix])[patch_idxs1]
+        # Find the point cv2_ix in s2 that is closest to cv1_ix
+        cv2_ix = nearest_neighbors_v1_to_v2[cv1_ix]
+        patch_idxs2 = np.where(mask2[cv2_ix]==1)[0]
+        neigh_cv2 = np.array(neigh2[cv2_ix])[patch_idxs2]
+
+        patch_v1 = v1[neigh_cv1]
+        patch_v2 = v2[neigh_cv2]
+        patch_n1 = n1[neigh_cv1]
+        patch_n2 = n2[neigh_cv2]
+
+        patch_kdt = cKDTree(patch_v1)
+        p_dists_v2_to_v1, p_nearest_neighbor_v2_to_v1 = patch_kdt.query(patch_v2)
+        patch_kdt = cKDTree(patch_v2)
+        p_dists_v1_to_v2, p_nearest_neighbor_v1_to_v2 = patch_kdt.query(patch_v1)
+        
+        # First v1->v2
+        neigh_cv1_p = p_nearest_neighbor_v1_to_v2
+        comp1 = [np.dot(patch_n1[x], -patch_n2[neigh_cv1_p][x]) for x in range(len(patch_n1))]
+        comp1 = np.multiply(comp1, np.exp(-w * np.square(p_dists_v1_to_v2)))
+        # Use 10 rings such that each ring has equal weight in shape complementarity
+        comp_rings1_25 = np.zeros(num_rings)
+        comp_rings1_50 = np.zeros(num_rings)
+
+        patch_rho1 = np.array(rho1[cv1_ix])[patch_idxs1]
+        for ring in range(num_rings):
+            scale = scales[ring]
+            members = np.where((patch_rho1 >= scales[ring]) & (patch_rho1 < scales[ring + 1]))
+            if len(members[0]) == 0:
+                comp_rings2_25[ring] = 0.0
+                comp_rings2_50[ring] = 0.0
+            else:
+                comp_rings1_25[ring] = np.percentile(comp1[members], 25)
+                comp_rings1_50[ring] = np.percentile(comp1[members], 50)
+        
+        # Now v2->v1
+        neigh_cv2_p = p_nearest_neighbor_v2_to_v1
+        comp2 = [np.dot(patch_n2[x], -patch_n1[neigh_cv2_p][x]) for x in range(len(patch_n2))]
+        comp2 = np.multiply(comp2, np.exp(-w * np.square(p_dists_v2_to_v1)))
+        # Use 10 rings such that each ring has equal weight in shape complementarity
+        comp_rings2_25 = np.zeros(num_rings)
+        comp_rings2_50 = np.zeros(num_rings)
+
+        # Apply mask to patch rho coordinates. 
+        patch_rho2 = np.array(rho2[cv2_ix])[patch_idxs2]
+        for ring in range(num_rings):
+            scale = scales[ring]
+            members = np.where((patch_rho2 >= scales[ring]) & (patch_rho2 < scales[ring + 1]))
+            if len(members[0]) == 0:
+                comp_rings2_25[ring] = 0.0
+                comp_rings2_50[ring] = 0.0
+            else:
+                comp_rings2_25[ring] = np.percentile(comp2[members], 25)
+                comp_rings2_50[ring] = np.percentile(comp2[members], 50)
+
+        v1_sc[0,cv1_ix,:] = comp_rings1_25
+        v2_sc[0,cv2_ix,:] = comp_rings2_25
+        v1_sc[1,cv1_ix,:] = comp_rings1_50
+        v2_sc[1,cv2_ix,:] = comp_rings2_50
+
+
+    return v1_sc, v2_sc
 
 
 def normalize_electrostatics(in_elec):
