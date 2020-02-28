1c7c7ea5b9b3f909a8ac00ffe0a4640dbb5d7762
pablogainza <pablo.gainza@gmail.com>
2020-02-28 09:26:34 +0000

    Added scripts to reproduce Masif site benchmark.

--- data/masif_site/data_prepare_one.sh
+++ data/masif_site/data_prepare_one.sh
@@ -6,7 +6,6 @@ export masif_matlab
 PDB_ID=$(echo $1| cut -d"_" -f1)
 CHAIN1=$(echo $1| cut -d"_" -f2)
 CHAIN2=$(echo $1| cut -d"_" -f3)
-python $masif_source/data_preparation/00-pdb_download.py $1
-python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
-python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
+python -W ignore $masif_source/data_preparation/00-pdb_download.py $1
+python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
 python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
--- data/masif_site/reproduce_transient_benchmark.sh
+++ data/masif_site/reproduce_transient_benchmark.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cat lists/testing_transient.txt | while read line 
 do
-   # do something with $line here
     ./data_prepare_one.sh $line\_
 done
+
+./predict_site.sh -l lists/testing_transient.txt
+./color_site.sh -l lists/testing_transient.txt
