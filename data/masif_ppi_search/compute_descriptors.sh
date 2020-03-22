69d606221d7a77709d536c16198d0aaddf2fd267
Pablo Gainza <pablo.gainza@epfl.ch>
2020-03-22 12:40:04 +0100

    Removed unnecessary variables

--- data/masif_ppi_search/compute_descriptors.sh
+++ data/masif_ppi_search/compute_descriptors.sh
@@ -1,6 +1,5 @@
 masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source/
-masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ppi_search/
 python $masif_source/masif_ppi_search/masif_ppi_search_comp_desc.py nn_models.sc05.all_feat.custom_params $1 $2
