ad75548a73dbb36e87b50fa927f6e921b41881db
Pablo Gainza <pablo.gainza@epfl.ch>
2020-03-23 16:09:49 +0100

    Cleaned up file.

--- comparison/masif_ppi_search/masif_descriptors_nn/second_stage_masif.sh
+++ comparison/masif_ppi_search/masif_descriptors_nn/second_stage_masif.sh
@@ -3,6 +3,5 @@ masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-#module load gcc cuda cudnn mvapich2 openblas
-source /home/gainza/lpdi_fs/masif/tensorflow-1.12_on_cpu/bin/activate
-python $masif_source/masif_ppi_search/second_stage_alignment_nn.py ../../../data/masif_ppi_search $1 2000 1000 masif
+# Activate your environment here.
+python -W ignore $masif_source/masif_ppi_search/second_stage_alignment_nn.py ../../../data/masif_ppi_search $1 2000 1000 masif
