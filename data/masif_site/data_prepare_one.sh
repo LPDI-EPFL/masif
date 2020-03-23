ed90c20fd061a87342a3b03e30b80f96e650003f
Pablo Gainza <pablo.gainza@epfl.ch>
2020-03-23 14:26:43 +0100

    Added proper handling of two chains or one when preparing a structure in masif site.

--- data/masif_site/data_prepare_one.sh
+++ data/masif_site/data_prepare_one.sh
@@ -10,6 +10,7 @@ then
 	PPI_PAIR_ID=$3
 	PDB_ID=$(echo $PPI_PAIR_ID| cut -d"_" -f1)
 	CHAIN1=$(echo $PPI_PAIR_ID| cut -d"_" -f2)
+	CHAIN2=$(echo $PPI_PAIR_ID| cut -d"_" -f3)
 	FILENAME=$2
 	mkdir -p data_preparation/00-raw_pdbs/
 	cp $FILENAME data_preparation/00-raw_pdbs/$PDB_ID\.pdb
@@ -17,7 +18,16 @@ else
 	PPI_PAIR_ID=$1
 	PDB_ID=$(echo $PPI_PAIR_ID| cut -d"_" -f1)
 	CHAIN1=$(echo $PPI_PAIR_ID| cut -d"_" -f2)
+	CHAIN2=$(echo $PPI_PAIR_ID| cut -d"_" -f3)
 	python -W ignore $masif_source/data_preparation/00-pdb_download.py $PPI_PAIR_ID
 fi
-python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
+
+if [ -z $CHAIN2 ]
+then
+    echo "Empty"
+    python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
+else
+    python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1
+    python -W ignore $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
+fi
 python $masif_source/data_preparation/04-masif_precompute.py masif_site $PPI_PAIR_ID
