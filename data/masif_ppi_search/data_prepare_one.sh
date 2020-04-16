f12aadfe1162e947635faf2de438d59c25b8e09a
Pablo Gainza <pablo.gainza@gmail.com>
2020-04-16 22:29:38 +0000

    Added a set of scripts to reproduce the benchmark from the paper.

--- data/masif_ppi_search/data_prepare_one.sh
+++ data/masif_ppi_search/data_prepare_one.sh
@@ -8,4 +8,5 @@ CHAIN2=$(echo $1| cut -d"_" -f3)
 python $masif_source/data_preparation/00-pdb_download.py $1 
 python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN1 
 python $masif_source/data_preparation/01-pdb_extract_and_triangulate.py $PDB_ID\_$CHAIN2
+python $masif_source/data_preparation/04-masif_precompute.py masif_site $1
 python $masif_source/data_preparation/04-masif_precompute.py masif_ppi_search $1
