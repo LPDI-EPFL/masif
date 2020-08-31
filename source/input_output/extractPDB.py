f691c732d468d9e08539f92c8761f08392443409
Pablo Gainza <pablo.gainza@epfl.ch>
2020-08-31 13:19:20 +0200

    Added support for nonstandard modified aminoacids (e.g. MSE), using code contributed by JOMIMC
    Also added the code '1' or '2' for alternate states of the same molecule.
    
    Also removed some old unused parameters.

--- source/input_output/extractPDB.py
+++ source/input_output/extractPDB.py
@@ -5,17 +5,34 @@ Released under an Apache License 2.0
 """
 from Bio.PDB import *
 
+from Bio.SeqUtils import IUPACData
+PROTEIN_LETTERS = [x.upper() for x in IUPACData.protein_letters_3to1.keys()]
+
 # Exclude disordered atoms.
 class NotDisordered(Select):
     def accept_atom(self, atom):
-        return not atom.is_disordered() or atom.get_altloc() == "A"
+        return not atom.is_disordered() or atom.get_altloc() == "A"  or atom.get_altloc() == "1" 
+
+
+def find_modified_amino_acids(path):
+    """
+    Contributed by github user jomimc - find modified amino acids in the PDB (e.g. MSE)
+    """
+    res_set = set()
+    for line in open(path, 'r'):
+        if line[:6] == 'SEQRES':
+            for res in line.split()[4:]:
+                res_set.add(res)
+    for res in list(res_set):
+        if res in PROTEIN_LETTERS:
+            res_set.remove(res)
+    return res_set
+
 
 def extractPDB(
-    infilename, outfilename, chain_ids=None, includeWaters=False, invert=False
+    infilename, outfilename, chain_ids=None 
 ):
     # extract the chain_ids from infilename and save in outfilename. 
-    # includeWaters: deprecated parameter, include the crystallographic waters (should not be used). 
-    # invert: Select all chains EXCEPT those in chain_ids.
     parser = PDBParser(QUIET=True)
     struct = parser.get_structure(infilename, infilename)
     model = Selection.unfold_entities(struct, "M")[0]
@@ -26,23 +43,24 @@ def extractPDB(
     structBuild.init_seg(" ")
     structBuild.init_model(0)
     outputStruct = structBuild.get_structure()
+
+    # Load a list of non-standard amino acid names -- these are
+    # typically listed under HETATM, so they would be typically
+    # ignored by the orginal algorithm
+    modified_amino_acids = find_modified_amino_acids(infilename)
+
     for chain in model:
         if (
             chain_ids == None
-            or (chain.get_id() in chain_ids and not invert)
-            or invert == True
+            or chain.get_id() in chain_ids
         ):
             structBuild.init_chain(chain.get_id())
             for residue in chain:
                 het = residue.get_id()
-                if not invert:
-                    if het[0] == " " or (het[0] == "W" and includeWaters):
-                        outputStruct[0][chain.get_id()].add(residue)
-                else:
-                    if (het[0] == "W" and includeWaters) or (
-                        chain.get_id() not in chain_ids
-                    ):
-                        outputStruct[0][chain.get_id()].add(residue)
+                if het[0] == " ":
+                    outputStruct[0][chain.get_id()].add(residue)
+                elif het[0][-3:] in modified_amino_acids:
+                    outputStruct[0][chain.get_id()].add(residue)
 
     # Output the selected residues
     pdbio = PDBIO()
