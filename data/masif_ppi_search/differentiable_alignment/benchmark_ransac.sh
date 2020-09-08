30fffcc77072c2d23c2d34ee697a6f49186a95db
Pablo Gainza <pablo.gainza@epfl.ch>
2020-09-08 10:16:17 +0200

    Training and testing of differentiable alignment

---
+++ data/masif_ppi_search/differentiable_alignment/benchmark_ransac.sh
@@ -0,0 +1,8 @@
+masif_root=$(git rev-parse --show-toplevel)
+masif_source=$masif_root/source/
+export PYTHONPATH=$PYTHONPATH:$masif_source
+PDB_ID=$(echo $1| cut -d"_" -f1)
+CHAIN1=$(echo $1| cut -d"_" -f2)
+CHAIN2=$(echo $1| cut -d"_" -f3)
+# Load your environment here. 
+python $masif_source/masif_ppi_search/differentiable_alignment/benchmark_ransac.py 
